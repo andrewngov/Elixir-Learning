6b27dfb9d6064b83af88b86b4fbd6fb72d9b7bf1
andrewngov <ngov.andrew@gmail.com>
2024-04-30 15:45:43 -0400

    Stateful Server Processes
    Lesson 24 and exercises
    Store state and do computations on server process.

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -27,6 +27,23 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  def route(%Conv{method: "GET", path: "/pledges/new"} = conv) do
+    Servy.PledgeController.new(conv)
+  end
+
+  def route(%Conv{method: "GET", path: "/404s"} = conv) do
+    counts = Servy.FourOhFourCounter.get_counts()
+    %{conv | status: 200, resp_body: inspect counts}
+  end
+
+  def route(%Conv{method: "POST", path: "/pledges"} = conv) do
+    Servy.PledgeController.create(conv, conv.params)
+  end
+
+  def route(%Conv{method: "GET", path: "/pledges"} = conv) do
+    Servy.PledgeController.index(conv)
+  end
+
   def put_content_length(conv) do
     headers = Map.put(conv.resp_headers, "Content-Length", String.length(conv.resp_body))
     %{conv | resp_headers: headers}
---
+++ servy/lib/servy/pledge_controller.ex
@@ -0,0 +1,21 @@
+defmodule Servy.PledgeController do
+  import Servy.View
+
+  def create(conv, %{"name" => name, "amount" => amount}) do
+    # Sends the pledge to the external service and caches it
+    create_pledge(name, String.to_integer(amount))
+
+    %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!" }
+  end
+
+  def index(conv) do
+    # Gets the recent pledges from the cache
+    pledges = Servy.PledgeServer.recent_pledges()
+
+    View.render(conv, "recent_pledges.eex", pledges: pledges)
+  end
+
+  def new(conv) do
+    View.render(conv, "new_pledges.eex")
+  end
+end
