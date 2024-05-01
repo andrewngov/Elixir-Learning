5b605b91b9978d4118ac97c8f68bd81c307d2750
andrewngov <ngov.andrew@gmail.com>
2024-04-30 22:55:04 -0400

    OTP GenServer
    Lesson 26 and exercises.
    Replaced our code that started a server process with GenServer

--- servy/lib/servy/four_oh_four_counter.ex
+++ servy/lib/servy/four_oh_four_counter.ex
@@ -1,38 +1,7 @@
-defmodule Servy.GenericServer do
-  def start(callback_module, initial_state, name) do
-    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
-    Process.register(pid, name)
-    pid
-  end
-
-  #Helper functions
-  def call(pid, message) do
-    send pid, {:call, self(), message}
-  end
-
-  def cast(pid, message) do
-    send pid, {:cast, message}
-  end
-
-  #Server
-  def listen_loop(state, callback_module) do
-    receive do
-      {:call, sender, message} when is_pid(sender) ->
-        {response, new_state} = callback_module.handle_call(message, state)
-        send sender, {:response, response}
-        listen_loop(new_state, callback_module)
-      {:cast, message} ->
-        new_state = callback_module.handle_cast(message, state)
-        listen_loop(new_state, callback_module)
-      unexpected ->
-        IO.puts "Unexpected messaged: #{inspect unexpected}"
-        listen_loop(state, callback_module)
-    end
-  end
-end
-
 defmodule Servy.FourOhFourCounter do
-  @name :counter_server
+  @name :four_oh_four_counter
+
+  use GenServer
 
   alias Servy.GenericServer
   #STARTUP
@@ -43,23 +12,23 @@ defmodule Servy.FourOhFourCounter do
   #Client Interface
   def start do
     IO.puts "Starting the 404 counter..."
-    GenericServer.start(__MODULE__, %{}, @name)
+    GenServer.start(__MODULE__, %{}, @name)
   end
 
   def bump_count(path) do
-    GenericServer.call @name, {:bump_count, path}
+    GenServer.call @name, {:bump_count, path}
   end
 
   def get_counts do
-    GenericServer.call @name, :get_counts
+    GenServer.call @name, :get_counts
   end
 
   def get_count(path) do
-    GenericServer.call @name, {:get_count, path}
+    GenServer.call @name, {:get_count, path}
   end
 
   def reset do
-    GenericServer.cast @name, :reset
+    GenServer.cast @name, :reset
   end
 
   def handle_call(:bump_count, state) do
