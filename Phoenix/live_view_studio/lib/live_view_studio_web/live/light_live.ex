6099756b3651a353ff1345e4b569ca691600b368
andrewngov <ngov.andrew@gmail.com>
2024-05-06 01:48:12 -0400

    Dynamic Form
    Lesson 5
    Dynamically change price based on input changes.
    Add slider to light bar from first lesson and temperature button that change colour.

--- Phoenix/live_view_studio/lib/live_view_studio_web/live/light_live.ex
+++ Phoenix/live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, brightness: 10)
+    socket = assign(socket, brightness: 10, temp: "3000")
     {:ok, socket}
   end
 
@@ -11,7 +11,7 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Front Proch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}>
           <%= @brightness %>%
         </span>
       </div>
@@ -30,6 +30,23 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="rando">
         <img src="/images/fire.svg">
       </button>
+
+      <form phx-change="change-temp">
+        <div class="temps">
+          <%= for temp <- ["3000", "4000", "5000"] do %>
+            <div>
+              <input type="radio" id={temp} name="temp" value={temp}
+              checked={temp == @temp} />
+              <label for={temp}><%= temp %></label>
+            </div>
+          <% end %>
+        </div>
+      </form>
+
+      <form phx-change="update">
+        <input type="range" min="0" max="100"
+          name="brightness" value={@brightness} />
+      </form>
     </div>
     """
   end
@@ -58,4 +75,18 @@ defmodule LiveViewStudioWeb.LightLive do
     socket = assign(socket, brightness: Enum.random(0..100))
     {:noreply, socket}
   end
+
+  def handle_event("update", %{"brightness" => brightness}, socket) do
+    socket = assign(socket, brightness: String.to_integer(brightness))
+    {:noreply, socket}
+  end
+
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    socket = assign(socket, temp: temp)
+    {:noreply, socket}
+  end
+
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#FEFF66"
+  defp temp_color("5000"), do: "#99CCFF"
 end
