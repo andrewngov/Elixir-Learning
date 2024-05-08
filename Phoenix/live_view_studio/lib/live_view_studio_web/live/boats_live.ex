ee7f616d27256bad433d11a391cdeb99c1ff1369
andrewngov <ngov.andrew@gmail.com>
2024-05-08 00:40:38 -0400

    Function Components
    Lesson 10 and exercises.
    Extracted parts of heex template and put them into function components

--- Phoenix/live_view_studio/lib/live_view_studio_web/live/boats_live.ex
+++ Phoenix/live_view_studio/lib/live_view_studio_web/live/boats_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.BoatsLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Boats
+  import LiveViewStudioWeb.CustomComponents
 
   def mount(_params, _session, socket) do
     socket =
@@ -16,8 +17,56 @@ defmodule LiveViewStudioWeb.BoatsLive do
   def render(assigns) do
     ~H"""
     <h1>Daily Boat Rentals</h1>
+
+    <.promo expiration={2}>
+      Save 25% on rentals!
+      <:legal>
+        <Heroicons.exclamation_circle />
+        Limit 1 per party
+      </:legal>
+    </.promo>
+
     <div id="boats">
-      <form phx-change="filter">
+
+      <.filter_form filter={@filter} />
+
+      <div class="boats">
+        <.boat :for={boat <- @boats} boat={boat} />
+      </div>
+    </div>
+
+    <.promo expiration={1}>
+      Hurry, only 3 boats left!
+    </.promo>
+    """
+  end
+
+  attr(:boat, LiveViewStudio.Boats.Boat, required: true)
+
+  def boat(assigns) do
+    ~H"""
+    <div class="boat">
+          <img src={@boat.image} />
+          <div class="content">
+            <div class="model">
+              <%= @boat.model %>
+            </div>
+            <div class="details">
+              <span class="price">
+                <%= @boat.price %>
+              </span>
+              <span class="type">
+                <%= @boat.type %>
+              </span>
+            </div>
+          </div>
+        </div>
+    """
+  end
+
+  def filter_form(assigns) do
+    ~H"""
+    <form phx-change="filter">
         <div class="filters">
           <select name="type">
             <%= Phoenix.HTML.Form.options_for_select(
@@ -40,25 +89,6 @@ defmodule LiveViewStudioWeb.BoatsLive do
           </div>
         </div>
       </form>
-      <div class="boats">
-        <div :for={boat <- @boats} class="boat">
-          <img src={boat.image} />
-          <div class="content">
-            <div class="model">
-              <%= boat.model %>
-            </div>
-            <div class="details">
-              <span class="price">
-                <%= boat.price %>
-              </span>
-              <span class="type">
-                <%= boat.type %>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     """
   end
 
